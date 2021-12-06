876bae5037d56587fde9a4910511ff2b88bfe3b6
Hyper <50504920+itsHyper@users.noreply.github.com>
2021-12-06 15:57:24 -0500

    Updated

--- server/main.lua
+++ server/main.lua
@@ -2,18 +2,15 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 
 RegisterServerEvent('qb-rental:rentalpapers')
-AddEventHandler('qb-rental:rentalpapers', function(plate, modelo, money)
+AddEventHandler('qb-rental:rentalpapers', function(plate, model, money)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    print(plate)
     local info = {}
     info.citizenid = Player.PlayerData.citizenid
     info.firstname = Player.PlayerData.charinfo.firstname
     info.lastname = Player.PlayerData.charinfo.lastname
     info.plate = plate
-    info.model = modelo
-
-    print(json.encode(info))
+    info.model = model
     TriggerClientEvent('inventory:client:ItemBox', src,  QBCore.Shared.Items["rentalpapers"], 'add')
     Player.Functions.AddItem('rentalpapers', 1, false, info)
     Player.Functions.RemoveMoney('bank', money, "vehicle-rental")
